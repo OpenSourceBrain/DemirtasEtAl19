16a4b7824aa0746fe6c26bf0d8006a63f0360f7a
Padraig  Gleeson <p.gleeson@gmail.com>
2020-08-13 18:08:29 +0100

    More ideas

--- Python/TestModel.py
+++ Python/TestModel.py
@@ -1,19 +1,47 @@
 
 from hbnm.model.dmf import Model
+from hbnm.io import Data
+
+
+from hbnm.model.utils import subdiag, linearize_map, normalize_sc, fisher_z
+
+def load_data(data):
+    fin = data.load('demirtas_neuron_2019.hdf5')
+    sc = fin['sc'].value
+    fc = fin['fc'].value
+    t1t2 = fin['t1wt2w'].value
+    fin.close()
+
+    # For left hemisphere, use first 180 indices
+    sc = normalize_sc(sc[:180,:180])
+    fc_obj = fc[:180,:180]
+    hmap = linearize_map(t1t2[:180])
+
+    return sc, hmap, fc_obj
 
 import numpy as np
 
-sc = np.array([[0.1,1],[0.5,0.1]])
+sc = np.array([[0.001,1],[0.001,0.001]])
+#sc = np.array([[1e-12]])
+output_dir = '.'
+input_dir = '../../../../../../git/hbnm/data/'
+data = Data(input_dir, output_dir)
+sc, hmap, fc_obj = load_data(data)
+    
 
-print('Structural connectivity:\n%s'%sc)
+num_pops = sc.shape[0]
+
+print('Structural connectivity for %i pops (%s):\n%s'%(num_pops, sc.shape, sc))
 
 model = Model(sc, g=0, norm_sc=True, hmap = None,
                  wee=(0.15, 0.), wei=(0.15, 0.),
                  syn_params=None, bold_params='obata',
                  verbose=True)
+                 
+#model._sigma = 0
             
 print('Created model')
-t = 2
+t = .2
 
 model.set_jacobian(compute_fic=True)
 
@@ -28,8 +56,22 @@ print('Integrated model')
 import matplotlib.pyplot as plt
 print(model.sim.r_E)
 
-plt.plot(model.sim.r_E.T)
-plt.plot(model.sim.r_I.T)
+vars = {'Rates':['r_E','r_I'],
+        'S variables':['S_E','S_I'],
+        'Currents':['I_E','I_I']}
 
+for v in vars:
+    fig = plt.figure()
+    fig.canvas.set_window_title(v)
+    plt.title(v)
+    for t in vars[v]:
+        a = eval('model.sim.%s'%t)
+        print(a.shape)
+        print(a[0].shape)
+        for i in range(num_pops):
+            plt.plot(a[i],label='%s[%i]'%(t,i))
+        
+    plt.legend()
+    
 plt.show()
 
